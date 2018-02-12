fe0c3cf56cda2646300e69656fd789a585444b06
vladimir <vcaraseni@gmail.com>
2018-02-12 13:44:47 +0200

    checking

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -84,6 +84,7 @@
                 </div>
 
                 <div class="links">
+                    <p>Test</p>
                     <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
