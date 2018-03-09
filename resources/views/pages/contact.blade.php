cc6830b42aadb380beb4e334988540c876c2ceaf
IC <himac!>
2018-03-09 15:49:59 +0200

    Contact page finished

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -11,42 +11,52 @@
                 <form action="">
                     <div class="row">
                         <div class="col-12 col-md-7">
-                            <label class="form_el_label"><span>Secteur</span></label>
-                            <select id="view_select" name="object_view" title="">
-                                <option value="0">Option 1</option>
-                                <option value="1">Option 2</option>
-                                <option value="2">Option 3</option>
-                                <option value="3">Option 4</option>
-                                <option value="4">Option 5</option>
-                            </select>
-                            <span>Veuillez spécifier le secteur que vous souhaitez contacter</span>
+                            <div class="form_group">
+                                <label class="form_el_label"><span>Secteur</span></label>
+                                <select id="view_select" name="object_view" title="">
+                                    <option value="0">Option 1</option>
+                                    <option value="1">Option 2</option>
+                                    <option value="2">Option 3</option>
+                                    <option value="3">Option 4</option>
+                                    <option value="4">Option 5</option>
+                                </select>
+                                <span class="form_placeholder">Veuillez spécifier le secteur que vous souhaitez contacter</span>
+                            </div>
                         </div>
                         <div class="col-12 col-md-7">
-                            <label class="form_el_label"><span>Email</span></label>
-                            <div class="input_container">
-                                <input type="email" name="emial" value="" placeholder="">
+                            <div class="form_group">
+                                <label class="form_el_label"><span>Email</span></label>
+                                <div class="input_container">
+                                    <input type="email" name="emial" value="" placeholder="">
+                                </div>
+                                <span class="form_placeholder">Veuillez entrer votre adresse e-mail</span>
                             </div>
-                            <span>Veuillez entrer votre adresse e-mail</span>
                         </div>
                         <div class="col-12">
-                            <label class="form_el_label"><span>Message</span></label>
-                            <div class="input_container">
-                                <textarea name="message" value="" placeholder=""></textarea>
+                            <div class="form_group">
+                                <label class="form_el_label"><span>Message</span></label>
+                                <div class="input_container">
+                                    <textarea name="message" value="" placeholder=""></textarea>
+                                </div>
+                                <span class="form_placeholder">Rédigez votre message ici.</span>
                             </div>
-                            <span>Rédigez votre message ici.</span>
                         </div>
-                        <div class="col-12 col-md-4">
+                        <div class="col-12">
                             <button type="submit" class="btn btn_blue">Envoyer</button>
-                            <p class="address">
-                                Fight Night Saint-Tropez
-                                2 Montée de la Citadelle
-                                1202 ST TROPEZ
-                            </p>
-                            <p class="tel">T. + 41 (0)22 365 10 10</p>
-                            <p class="tel">F. + 41 (0)22 365 10 20</p>
                         </div>
                     </div>
                 </form>
+                <div class="row">
+                    <div class="col-12 col-md-4">
+                        <p class="address">
+                            Fight Night Saint-Tropez<br>
+                            2 Montée de la Citadelle<br>
+                            1202 ST TROPEZ
+                        </p>
+                        <p class="tel">T. + 41 (0)22 365 10 10</p>
+                        <p class="tel">F. + 41 (0)22 365 10 20</p>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
