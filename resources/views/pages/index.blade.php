005fe6d63ff82cd7879f732c8e2a10519227cbea
IC <himac!>
2018-03-19 19:30:19 +0200

    Responsiveness finished

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -33,56 +33,7 @@
             <div class="countdown_container jeans_bg">
                 <div class="row">
                     <div class="col-12 col-md-7">
-                        <div class="countdown">
-                            <div class="countdown_item" id="days">
-                                <svg id="animated_days" viewbox="0 0 100 100">
-                                    <circle cx="50" cy="50" r="40" stroke="#ffffff" stroke-opacity="0.3" stroke-width="10" fill="#000000" fill-opacity="0"/>
-                                    <path id="progress_days" stroke-linecap="round" stroke-width="10" stroke="#ffffff" fill="none"
-                                          d="M50 10 a 40 40 0 0 1 0 80 a 40 40 0 0 1 0 -80">
-                                    </path>
-                                    <text class="count" x="50" y="50" font-family="proxima_nova_semibold" text-anchor="middle" dy="7" dx="7" fill="#ffffff">
-                                        <tspan class="value" x="45" y="50" font-size="30" text-anchor="middle">0</tspan>
-                                        <tspan x="50" y="50" dy="2.2em" font-size="8" text-anchor="middle">DAYS</tspan>
-                                    </text>
-                                </svg>
-                            </div>
-                            <div class="countdown_item" id="hours">
-                                <svg id="animated_hours" viewbox="0 0 100 100">
-                                    <circle cx="50" cy="50" r="40" stroke="#ffffff" stroke-opacity="0.3" stroke-width="10" fill="#000000" fill-opacity="0"/>
-                                    <path id="progress_hours" stroke-linecap="round" stroke-width="10" stroke="#ffffff" fill="none"
-                                          d="M50 10 a 40 40 0 0 1 0 80 a 40 40 0 0 1 0 -80">
-                                    </path>
-                                    <text class="count" x="50" y="50" font-family="proxima_nova_semibold" text-anchor="middle" dy="7" dx="7" fill="#ffffff">
-                                        <tspan class="value" x="45" y="50" font-size="30" text-anchor="middle">0</tspan>
-                                        <tspan x="50" y="50" dy="2.2em" font-size="8" text-anchor="middle">HOURS</tspan>
-                                    </text>
-                                </svg>
-                            </div>
-                            <div class="countdown_item" id="minutes">
-                                <svg id="animated_minutes" viewbox="0 0 100 100">
-                                    <circle cx="50" cy="50" r="40" stroke="#ffffff" stroke-opacity="0.3" stroke-width="10" fill="#000000" fill-opacity="0"/>
-                                    <path id="progress_minutes" stroke-linecap="round" stroke-width="10" stroke="#ffffff" fill="none"
-                                          d="M50 10 a 40 40 0 0 1 0 80 a 40 40 0 0 1 0 -80">
-                                    </path>
-                                    <text class="count" x="50" y="50" font-family="proxima_nova_semibold" text-anchor="middle" dy="7" dx="7" fill="#ffffff">
-                                        <tspan class="value" x="45" y="50" font-size="30" text-anchor="middle">0</tspan>
-                                        <tspan x="50" y="50" dy="2.2em" font-size="8" text-anchor="middle">MINUTES</tspan>
-                                    </text>
-                                </svg>
-                            </div>
-                            <div class="countdown_item" id="seconds">
-                                <svg id="animated_seconds" viewbox="0 0 100 100">
-                                    <circle cx="50" cy="50" r="40" stroke="#ffffff" stroke-opacity="0.3" stroke-width="10" fill="#000000" fill-opacity="0"/>
-                                    <path id="progress_seconds" stroke-linecap="round" stroke-width="10" stroke="#ffffff" fill="none"
-                                          d="M50 10 a 40 40 0 0 1 0 80 a 40 40 0 0 1 0 -80">
-                                    </path>
-                                    <text class="count" x="50" y="50" font-family="proxima_nova_semibold" text-anchor="middle" dy="7" dx="7" fill="#ffffff">
-                                        <tspan class="value" x="45" y="50" font-size="30" text-anchor="middle">0</tspan>
-                                        <tspan x="50" y="50" dy="2.2em" font-size="8" text-anchor="middle">SECONDS</tspan>
-                                    </text>
-                                </svg>
-                            </div>
-                        </div>
+                        @include('includes.countdown')
                     </div>
                     <div class="col-12 col-md-5">
                         <div class="title_container">
