90c538ccb537847d67b0be67c1ddf0238503601f
Alex <a.loscinin@voodoo-studio.ch>
2018-03-27 21:34:07 +0300

    edit page editions (add content)

--- resources/lang/en/lang.php
+++ resources/lang/en/lang.php
@@ -31,25 +31,25 @@ return [
 
     // Editions page
     'editions_block_2013'   => '<h3 class="edition_statistics"><span>1<sup>ere</sup> edition</span> 200 000 tv spectateurs</h3>
-                            <p class="edition_description">Cette soirée unique est lB première rencontre poings-pieds Bvec les plus grBnds noms du Kick-Boxing et du Pro-Fight, sur le ring instBllé P lB CitBdelle de SBint-Tropez. PBrrBiné pBr M. Drucker, cet événement BurB égBlement une dimension cBritBtive.</p>
-                            <p class="edition_person"><span>Invité</span>Sylvester Stallone - Sebastien Chabbal</p>
-                            <p class="edition_person"><span>Combats</span>Jerôme Le banner</p>',
-    'editions_block_2014'   => '<h3 class="edition_statistics"><span>2<sup>ere</sup> edition</span> 300 000 tv spectateurs</h3>
-                            <p class="edition_description">La soirée c\' est très bien passée et <b>Jerome le Banner, Freddy Kemayo</b> et <b>Nicolas Wamba</b> ce sont tous les trois imposés par KO au second round !</p>
-                            <p class="edition_person"><span>Invité</span>Vincent Lagaf - DBvid Ginola - Enrico Macias</p>
-                            <p class="edition_person"><span>Combats</span>Jerôme Le banner</p>',
-    'editions_block_2015'   => '<h3 class="edition_statistics"><span>2<sup>ere</sup> edition</span> 300 000 tv spectateurs</h3>
-                            <p class="edition_description">La soirée c\' est très bien passée et <b>Jerome le Banner, Freddy Kemayo</b> et <b>Nicolas Wamba</b> ce sont tous les trois imposés par KO au second round !</p>
-                            <p class="edition_person"><span>Invité</span>Vincent Lagaf - DBvid Ginola - Enrico Macias</p>
-                            <p class="edition_person"><span>Combats</span>Jerôme Le banner</p>',
-    'editions_block_2016'   => '<h3 class="edition_statistics"><span>2<sup>ere</sup> edition</span> 300 000 tv spectateurs</h3>
-                            <p class="edition_description">La soirée c\' est très bien passée et <b>Jerome le Banner, Freddy Kemayo</b> et <b>Nicolas Wamba</b> ce sont tous les trois imposés par KO au second round !</p>
-                            <p class="edition_person"><span>Invité</span>Vincent Lagaf - DBvid Ginola - Enrico Macias</p>
-                            <p class="edition_person"><span>Combats</span>Jerôme Le banner</p>',
-    'editions_block_2017'   => '<h3 class="edition_statistics"><span>2<sup>ere</sup> edition</span> 300 000 tv spectateurs</h3>
-                            <p class="edition_description">La soirée c\' est très bien passée et <b>Jerome le Banner, Freddy Kemayo</b> et <b>Nicolas Wamba</b> ce sont tous les trois imposés par KO au second round !</p>
-                            <p class="edition_person"><span>Invité</span>Vincent Lagaf - DBvid Ginola - Enrico Macias</p>
-                            <p class="edition_person"><span>Combats</span>Jerôme Le banner</p>',
+                            <p class="edition_description">Cette soirée unique est la première rencontre poings-pieds avec les plus grands noms du Kick-Boxing et du Pro-Fight, sur le ring installé à la Citadelle de Saint-Tropez. Parrainé par M. Drucker, cet événement aura également une dimension caritative. </p>
+                            <p class="edition_person"><span>Guest</span>Sylvester Stallone - Sébastien Chabbal</p>
+                            <p class="edition_person"><span>Fighter</span>Jérôme Le Banner</p>',
+    'editions_block_2014'   => '<h3 class="edition_statistics"><span>2<sup>eme</sup> edition</span> 300 000 tv spectateurs</h3>
+                            <p class="edition_description">La soirée c\'est très bien passée et <b>Jérôme Le Banner, Freddy Kemayo</b> et <b>Nicolas Wamba</b> ce sont tous les trois imposés par KO au second round !</p>
+                            <p class="edition_person"><span>Guest</span>Vincent Lagaf - David Ginola - Enrico Macias</p>
+                            <p class="edition_person"><span>Fighter </span>Jérôme Le Banner</p>',
+    'editions_block_2015'   => '<h3 class="edition_statistics"><span>3<sup>eme</sup> edition</span> 300 000 tv spectateurs</h3>
+                            <p class="edition_description">Bien que de nombreux combats de prestige se tiendront sur le ring, toute lʼattention sera portée sur Jérôme Le Banner, véritable porte-étendard des sports de combat en France qui tirera sa révérence au terme de la soirée. Lʼoccasion pour nous de le saluer et de lui souhaiter le meilleur pour la suite...</p>
+                            <p class="edition_person"><span>Guest</span>Joey Starr - Mathieu Kassovitz</p>
+                            <p class="edition_person"><span>Fighter </span>Jérôme Le Banner last Fight </p>',
+    'editions_block_2016'   => '<h3 class="edition_statistics"><span>4<sup>eme</sup> edition</span> 300 000 tv spectateurs</h3>
+                            <p class="edition_description">On note particulièrement la mise en place d\'un tournoi à 4 dans la division des poids-lourds, un terme plus que jamais approprié puisque le plus léger de ces 4 combattants pèse plus de 120kg ! Comme annoncé sur l\'affiche, c\'est un "+500kg tournament" qui sera proposé, une chose est sure, la citadelle va trembler !</p>
+                            <p class="edition_person"><span>Guest</span>Mickey Rourke..</p>
+                            <p class="edition_person"><span>Fighter </span>Yohan Lidon - Daniel Sam</p>',
+    'editions_block_2017'   => '<h3 class="edition_statistics"><span>5<sup>eme</sup> edition</span> 350 000 tv spectateurs</h3>
+                            <p class="edition_description">La cinquième édition voit s\'opposer Grégory Tony et Bob SAPP. Yohan Lidon, lui défendra sa ceinture mondiale WKN face à l\'Allemand Florian Kroger. Un show spectaculaire aussi bien sur le ring qu\'en dehors devant un parterre de VIP. </p>
+                            <p class="edition_person"><span>Guest</span>Jean-Luc Reichmann - Jean Rock...</p>
+                            <p class="edition_person"><span>Fighter </span>Bob Sapp - Yohan Lidon</p>',
 
 
     // Ticketing page
