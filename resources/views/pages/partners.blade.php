0af682b84dc36e9cf26a3924c645271d5173c96e
IC <himac!>
2018-03-31 23:48:32 +0300

    about, partners and editions page fixes

--- resources/views/pages/partners.blade.php
+++ resources/views/pages/partners.blade.php
@@ -4,251 +4,251 @@
     <section class="partners_section">
         <div class="container-fluid">
             <div class="img_container">
-                <img src="/img/presse/presse_img.jpg" alt="About us banner">
+                <img src="/img/partners/partners.jpg" alt="About us banner">
             </div>
             <div class="partners_container jeans_bg_ragged">
                 <h1 class="title--translated">Partenaires</h1>
                 <h2>Partenaires historiques</h2>
                 <div class="partners_carousel">
                     <div class="carousel_item">
-                        <a href="#">
+                        {{--<a href="#">--}}
                             <div class="partner">
                                 <img src="/img/partners/partenaires_historiques/2logos.png" alt="logo Pastis Hôtel + Strend ">
                             </div>
-                        </a>
+                        {{--</a>--}}
                     </div>
                     <div class="carousel_item">
-                        <a href="#">
+                        {{--<a href="#">--}}
                             <div class="partner">
                                 <img src="/img/partners/partenaires_historiques/3logos.png" alt="logo Sporting + Maître Vignerons + Sylvain Costa ">
                             </div>
-                        </a>
+                        {{--</a>--}}
                     </div>
                     <div class="carousel_item">
-                        <a href="#">
+                        {{--<a href="#">--}}
                             <div class="partner">
                                 <img src="/img/partners/partenaires_historiques/3logos2.png" alt="logo belle rose hotel + Tartane + San Pellegrino ">
                             </div>
-                        </a>
+                        {{--</a>--}}
                     </div>
                     <div class="carousel_item">
-                        <a href="#">
+                        {{--<a href="#">--}}
                             <div class="partner">
                                 <img src="/img/partners/partenaires_historiques/blondesttropez.png" alt="logo Blond of saint tropez">
                             </div>
-                        </a>
+                        {{--</a>--}}
                     </div>
                     <div class="carousel_item">
-                        <a href="#">
+                        {{--<a href="#">--}}
                             <div class="partner">
                                 <img src="/img/partners/partenaires_historiques/cafedeparis.png" alt="logo Café paris">
                             </div>
-                        </a>
+                        {{--</a>--}}
                     </div>
                     <div class="carousel_item">
-                        <a href="#">
+                        {{--<a href="#">--}}
                             <div class="partner">
                                 <img src="/img/partners/partenaires_historiques/edenplage.png" alt="logo Eden plage">
                             </div>
-                        </a>
+                        {{--</a>--}}
                     </div>
                     <div class="carousel_item">
-                        <a href="#">
+                        {{--<a href="#">--}}
                             <div class="partner">
                                 <img src="/img/partners/partenaires_historiques/icetropez.png" alt="logo Ice tropez">
                             </div>
-                        </a>
+                        {{--</a>--}}
                     </div>
                     <div class="carousel_item">
-                        <a href="#">
+                        {{--<a href="#">--}}
                             <div class="partner">
                                 <img src="/img/partners/partenaires_historiques/manuclim.png" alt="logo Manu clim">
                             </div>
-                        </a>
+                        {{--</a>--}}
                     </div>
                     <div class="carousel_item">
-                        <a href="#">
+                        {{--<a href="#">--}}
                             <div class="partner">
                                 <img src="/img/partners/partenaires_historiques/masbellevue.png" alt="logo Mas bellevue">
                             </div>
-                        </a>
+                        {{--</a>--}}
                     </div>
                     <div class="carousel_item">
-                        <a href="#">
+                        {{--<a href="#">--}}
                             <div class="partner">
                                 <img src="/img/partners/partenaires_historiques/pastis.png" alt="logo Pastis">
                             </div>
-                        </a>
+                        {{--</a>--}}
                     </div>
                     <div class="carousel_item">
-                        <a href="#">
+                        {{--<a href="#">--}}
                             <div class="partner">
                                 <img src="/img/partners/partenaires_historiques/riviera_village.png" alt="logo Riviera village">
                             </div>
-                        </a>
+                        {{--</a>--}}
                     </div>
                     <div class="carousel_item">
-                        <a href="#">
+                        {{--<a href="#">--}}
                             <div class="partner">
                                 <img src="/img/partners/partenaires_historiques/sezz.png" alt="logo Hôtel sezz">
                             </div>
-                        </a>
+                        {{--</a>--}}
                     </div>
                     <div class="carousel_item">
-                        <a href="#">
+                        {{--<a href="#">--}}
                             <div class="partner">
                                 <img src="/img/partners/partenaires_historiques/torchia.png" alt="logo Torchia">
                             </div>
-                        </a>
+                        {{--</a>--}}
                     </div>
                     <div class="carousel_item">
-                        <a href="#">
+                        {{--<a href="#">--}}
                             <div class="partner">
                                 <img src="/img/partners/partenaires_historiques/universaldecoration.png" alt="logo universal deco">
                             </div>
-                        </a>
+                        {{--</a>--}}
                     </div>
                     <div class="carousel_item">
-                        <a href="#">
+                        {{--<a href="#">--}}
                             <div class="partner">
                                 <img src="/img/partners/partenaires_historiques/viproom.png" alt="logo VIP Room">
                             </div>
-                        </a>
+                        {{--</a>--}}
                     </div>
                 </div>
                 <h2>Grands partenaires</h2>
                 <div class="partners_carousel">
                     <div class="carousel_item">
-                        <a href="#">
+                        {{--<a href="#">--}}
                             <div class="partner">
                                 <img src="/img/partners/grands_partenaires/bagatelle.png" alt="logo Bagatelle">
                             </div>
-                        </a>
+                        {{--</a>--}}
                     </div>
                     <div class="carousel_item">
-                        <a href="#">
+                        {{--<a href="#">--}}
                             <div class="partner">
                                 <img src="/img/partners/grands_partenaires/heraclee.png" alt="logo Heraclee">
                             </div>
-                        </a>
+                        {{--</a>--}}
                     </div>
                     <div class="carousel_item">
-                        <a href="#">
+                        {{--<a href="#">--}}
                             <div class="partner">
                                 <img src="/img/partners/grands_partenaires/madinevent.png" alt="logo Mad in event">
                             </div>
-                        </a>
+                        {{--</a>--}}
                     </div>
                     <div class="carousel_item">
-                        <a href="#">
+                        {{--<a href="#">--}}
                             <div class="partner">
                                 <img src="/img/partners/grands_partenaires/mairie.png" alt="logo Mairie de saint-tropez">
                             </div>
-                        </a>
+                        {{--</a>--}}
                     </div>
                     <div class="carousel_item">
-                        <a href="#">
+                        {{--<a href="#">--}}
                             <div class="partner">
                                 <img src="/img/partners/grands_partenaires/moorea.png" alt="logo Moorea">
                             </div>
-                        </a>
+                        {{--</a>--}}
                     </div>
                     <div class="carousel_item">
-                        <a href="#">
+                        {{--<a href="#">--}}
                             <div class="partner">
                                 <img src="/img/partners/your_logo.png" alt="Your logo">
                             </div>
-                        </a>
+                        {{--</a>--}}
                     </div>
                 </div>
                 <h2>Partenaires privilégiés</h2>
                 <div class="partners_carousel">
                     <div class="carousel_item">
-                        <a href="#">
+                        {{--<a href="#">--}}
                             <div class="partner">
                                 <img src="/img/partners/partenaires_privilégiés/edenplage.png" alt="logo Eden plage">
                             </div>
-                        </a>
+                        {{--</a>--}}
                     </div>
                     <div class="carousel_item">
-                        <a href="#">
+                        {{--<a href="#">--}}
                             <div class="partner">
                                 <img src="/img/partners/partenaires_privilégiés/mapauto.png" alt="logo Map auto">
                             </div>
-                        </a>
+                        {{--</a>--}}
                     </div>
                     <div class="carousel_item">
-                        <a href="#">
+                        {{--<a href="#">--}}
                             <div class="partner">
                                 <img src="/img/partners/partenaires_privilégiés/opera.png" alt="logo Opera">
                             </div>
-                        </a>
+                        {{--</a>--}}
                     </div>
                     <div class="carousel_item">
-                        <a href="#">
+                        {{--<a href="#">--}}
                             <div class="partner">
                                 <img src="/img/partners/your_logo.png" alt="Your logo">
                             </div>
-                        </a>
+                        {{--</a>--}}
                     </div>
                     <div class="carousel_item">
-                        <a href="#">
+                        {{--<a href="#">--}}
                             <div class="partner">
                                 <img src="/img/partners/your_logo.png" alt="Your logo">
                             </div>
-                        </a>
+                        {{--</a>--}}
                     </div>
                     <div class="carousel_item">
-                        <a href="#">
+                        {{--<a href="#">--}}
                             <div class="partner">
                                 <img src="/img/partners/your_logo.png" alt="Your logo">
                             </div>
-                        </a>
+                        {{--</a>--}}
                     </div>
                 </div>
                 <h2>Partenaires médias</h2>
                 <div class="partners_carousel">
                     <div class="carousel_item">
-                        <a href="#">
+                        {{--<a href="#">--}}
                             <div class="partner">
                                 <img src="/img/partners/partenaires_medias/canalsport.png" alt="logo Canal sport">
                             </div>
-                        </a>
+                        {{--</a>--}}
                     </div>
                     <div class="carousel_item">
-                        <a href="#">
+                        {{--<a href="#">--}}
                             <div class="partner">
                                 <img src="/img/partners/your_logo.png" alt="Your logo">
                             </div>
-                        </a>
+                        {{--</a>--}}
                     </div>
                     <div class="carousel_item">
-                        <a href="#">
+                        {{--<a href="#">--}}
                             <div class="partner">
                                 <img src="/img/partners/your_logo.png" alt="Your logo">
                             </div>
-                        </a>
+                        {{--</a>--}}
                     </div>
                     <div class="carousel_item">
-                        <a href="#">
+                        {{--<a href="#">--}}
                             <div class="partner">
                                 <img src="/img/partners/your_logo.png" alt="Your logo">
                             </div>
-                        </a>
+                        {{--</a>--}}
                     </div>
                     <div class="carousel_item">
-                        <a href="#">
+                        {{--<a href="#">--}}
                             <div class="partner">
                                 <img src="/img/partners/your_logo.png" alt="Your logo">
                             </div>
-                        </a>
+                        {{--</a>--}}
                     </div>
                     <div class="carousel_item">
-                        <a href="#">
+                        {{--<a href="#">--}}
                             <div class="partner">
                                 <img src="/img/partners/your_logo.png" alt="Your logo">
                             </div>
-                        </a>
+                        {{--</a>--}}
                     </div>
                 </div>
             </div>
@@ -258,21 +258,52 @@
 
 @section('js')
     <script>
-        $('.partners_carousel').flickity({
-            // options
-            cellAlign: 'left',
-            cellSelector: '.carousel_item',
-            prevNextButtons: true,
-            contain: true,
-            wrapAround: true,
-            pageDots: false,
-            // autoPlay: true,
-            arrowShape: {
-                x0: 15,
-                x1: 65, y1: 50,
-                x2: 70, y2: 45,
-                x3: 25
-            }
-        });
+        window.setTimeout(initCarousel(), 2000);
+
+        function initCarousel() {
+//            $('.partners_carousel').flickity({
+//                // options
+//                cellAlign: 'left',
+//                cellSelector: '.carousel_item',
+//                prevNextButtons: true,
+//                contain: true,
+//                wrapAround: true,
+//                pageDots: false,
+//                autoPlay: true,
+//                arrowShape: {
+//                    x0: 15,
+//                    x1: 65, y1: 50,
+//                    x2: 70, y2: 45,
+//                    x3: 25
+//                }
+//            });
+            $('.partners_carousel').slick({
+                infinite: true,
+                slidesToShow: 5,
+                slidesToScroll: 1,
+                autoplay: true,
+                autoplaySpeed: 3000,
+                prevArrow: '<button type="button" class="slick-prev"><img src="/img/arrow_left.png" alt="Slide to previous"></button>',
+                nextArrow: '<button type="button" class="slick-next"><img src="/img/arrow_right.png" alt="Slide to previous"></button>',
+                responsive: [
+                    {
+                        breakpoint: 768,
+                        settings: {
+                            arrows: false,
+                            centerPadding: '40px',
+                            slidesToShow: 4
+                        }
+                    },
+                    {
+                        breakpoint: 480,
+                        settings: {
+                            arrows: false,
+                            centerPadding: '40px',
+                            slidesToShow: 3
+                        }
+                    }
+                ]
+            });
+        }
     </script>
 @stop
