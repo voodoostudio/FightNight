db5b838a1d4b7039cc858100d94f4cbfa9965ef6
Alex <a.loscinin@voodoo-studio.ch>
2018-04-05 16:59:38 +0300

    press edit

--- resources/views/pages/press.blade.php
+++ resources/views/pages/press.blade.php
@@ -76,7 +76,7 @@
             @endphp
         <!-- Modal -->
             <div class="modal fade" id="image_{{ $item->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog" role="document">
+                <div class="modal-dialog modal-dialog-centered" role="document">
                     <div class="modal-content">
                         <div class="modal-body">
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
