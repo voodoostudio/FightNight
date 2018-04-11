f8eb67944f17b46a8ed1e5a47270054876f91f5f
Alex <a.loscinin@voodoo-studio.ch>
2018-04-11 12:23:12 +0300

    tickets map

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -129,7 +129,7 @@ class PagesController extends Controller
         $content = curl_exec($ch);
         curl_close($ch);
 
-        $content = str_replace('/static/','https://static-ticketing.weezevent.net/static/', strpos($content, 'widget_main.css'));
+        $content = str_replace('/static/0efd6b/css/widget/widget_main.css','https://static-ticketing.weezevent.net/static/0efd6b/css/widget/widget_main.css', $content);
         $content = str_replace('images/billetterie/securite_info.gif','https://static-ticketing.weezevent.net/images/billetterie/securite_info.gif', $content);
 
         if($name == "salons") {
